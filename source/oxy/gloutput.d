37d98fd3e62ad2d228e456de8ddee50b5ecc4af7
nucular <nucular@users.noreply.github.com>
2016-01-24 22:47:32 +0100

    Start working on fade-out (broken)

--- source/oxy/gloutput.d
+++ source/oxy/gloutput.d
@@ -11,9 +11,17 @@ import oxy.buffer;
 
 class GLOutput : Output
 {
-  GLFWwindow* window;
-  double lastTime;
+  uint width = 800;
+  uint height = 800;
 
+  GLFWwindow* window;
+  GLuint[2] textures;
+  GLuint[2] depthbuffers;
+  GLuint[2] fbos;
+  size_t target = 0;
+  size_t prevtarget = 1;
+
+  double lastTime;
   Sample lastSample;
 
   this()
@@ -24,7 +32,7 @@ class GLOutput : Output
     if (!glfwInit())
       throw new Exception("glfwInit failed");
 
-    this.window = glfwCreateWindow(800, 800, "oxy", null, null);
+    this.window = glfwCreateWindow(this.width, this.height, "oxy", null, null);
     if (!window)
       throw new Exception("glfwCreateWindow failed");
 
@@ -36,6 +44,32 @@ class GLOutput : Output
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
     glLineWidth(2.0);
     glPointSize(2.0);
+
+    glGenTextures(2, this.textures.ptr);
+    glGenRenderbuffers(2, this.depthbuffers.ptr);
+    glGenFramebuffers(2, this.fbos.ptr);
+
+    for (int i = 0; i < this.textures.length; i++)
+    {
+      glBindTexture(GL_TEXTURE_2D, this.textures[i]);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, this.width, this.height,
+        0, GL_RGBA, GL_UNSIGNED_BYTE, null);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glBindTexture(GL_TEXTURE_2D, 0);
+
+      /*glBindRenderbuffer(GL_RENDERBUFFER, this.depthbuffers[i]);
+      glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, this.width, this.height);
+      glBindRenderbuffer(GL_RENDERBUFFER, 0);*/
+
+      glBindFramebuffer(GL_DRAW_FRAMEBUFFER, this.fbos[i]);
+      glFramebufferTexture(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, this.textures[i], 0);
+      if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        throw new Exception("glFramebufferTexture failed");
+      glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    }
 
     this.running = true;
   }
@@ -65,11 +99,29 @@ class GLOutput : Output
     glOrtho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glBegin(GL_LINE_STRIP);
 
+
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, this.fbos[this.target]);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0 + 0);
+    glBindTexture(GL_TEXTURE_2D, this.textures[this.prevtarget]);
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+    glTexCoord2f(0.0, 1.0); glVertex3f(0.0, 1.0, 0.0);
+    glTexCoord2f(1.0, 1.0); glVertex3f(1.0, 1.0, 0.0);
+    glTexCoord2f(1.0, 0.0); glVertex3f(1.0, 0.0, 0.0);
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+    glBegin(GL_LINE_STRIP);
     void drawSample(Sample sample)
     {
-      float sdist = pow(sample[0] - this.lastSample[0], 2) + pow(sample[1] - this.lastSample[1], 2);
+      float sdist = pow(sample[0] - this.lastSample[0], 2)
+        + pow(sample[1] - this.lastSample[1], 2);
       glColor4f(1.0, 1.0, 1.0, (1.0 - (sdist / 0.004)) * 1.0);
       glVertex3f(sample[0], sample[1], 0.0);
       this.lastSample = sample;
@@ -79,11 +131,18 @@ class GLOutput : Output
     Sample sample;
     while (this.samplebuffer.tryGet(sample))
       drawSample(sample);
-
     glEnd();
+
+
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, fbos[this.target]);
+    glBlitFramebuffer(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
 
     glfwSwapBuffers(this.window);
     glfwPollEvents();
+
+    swap(target, prevtarget);
 
     this.running = glfwWindowShouldClose(this.window) == 0;
   }
